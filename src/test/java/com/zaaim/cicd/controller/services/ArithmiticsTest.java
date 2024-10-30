ff0a492ea0a2a6d1363c9c4d04c405bead9c4338
Abdelhak Zaaim <abdelhakzammii@gmail.com>
2024-10-30 12:14:19 +0100

    add unit testes

--- src/test/java/com/zaaim/cicd/controller/services/ArithmiticsTest.java
+++ src/test/java/com/zaaim/cicd/controller/services/ArithmiticsTest.java
@@ -1,13 +1,31 @@
 package com.zaaim.cicd.controller.services;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ArithmiticsTest {
-   @Test
+
+    @Test
     public void testAdd() {
         int a = 1;
         int b = 2;
         int result = Arithmitics.add(a, b);
-        assert result == 3;
+        assertEquals(2, result);
+    }
+
+    @Test
+    public void testAddWithNegativeNumbers() {
+        int a = -1;
+        int b = -2;
+        int result = Arithmitics.add(a, b);
+        assertEquals(-3, result);
+    }
+
+    @Test
+    public void testAddWithZero() {
+        int a = 0;
+        int b = 0;
+        int result = Arithmitics.add(a, b);
+        assertEquals(0, result);
     }
 }
