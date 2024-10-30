c8cd552dbf084c0187beee5bbe76f20a56d40b8d
Abdelhak Zaaim <abdelhakzammii@gmail.com>
2024-10-30 15:04:03 +0100

    update

--- src/main/java/com/zaaim/cicd/controller/TestController.java
+++ src/main/java/com/zaaim/cicd/controller/TestController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/test")
     public String test() {
-        return "Hello Abdelhak Zaaim new commit test v2";
+        return "Hello Abdelhak Zaaim new commit test v3";
     }
 }
