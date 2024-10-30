51e2ca0cc5c59550e03af6bc74056136d88d3348
Abdelhak Zaaim <abdelhakzammii@gmail.com>
2024-10-30 12:01:10 +0100

    init

--- src/main/java/com/zaaim/cicd/controller/TestController.java
+++ src/main/java/com/zaaim/cicd/controller/TestController.java
@@ -1,3 +1,12 @@
 package com.zaaim.cicd.controller;
-   public class TestController {
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/test")
+    public String test() {
+        return "Hello World";
+    }
 }
