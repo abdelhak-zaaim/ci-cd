9635533845b25c53b7d3ea1ea02dbaf47ccfd53d
Abdelhak Zaaim <abdelhakzammii@gmail.com>
2024-11-02 11:56:12 +0100

    add methods [deploy]

--- src/main/java/com/zaaim/cicd/controller/TestController.java
+++ src/main/java/com/zaaim/cicd/controller/TestController.java
@@ -23,6 +23,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/test")
     public String test() {
-        return "the last deployment forversion 2";
+        return "<h1>Abdelhak Zaaim</h1>";
     }
 }
